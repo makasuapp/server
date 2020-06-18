70da164e62ea074642d1082d37923daf5ada6fcb
Owen Wang <wangfowen@gmail.com>
2020-06-18 17:14:00 -0400

    for now don't worry about forgery auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 # typed: ignore
 class ApplicationController < ActionController::Base
+  #TODO(auth)
+  protect_from_forgery with: :null_session
+
   def status
     render json: {message: "ok"}
   end
