2ead963c9d3b3de0609b4934d8131bdb234a42a1
Owen Wang <wangfowen@gmail.com>
2020-08-21 19:51:04 -0400

    add notification + data message

--- app/services/firebase.rb
+++ app/services/firebase.rb
@@ -12,11 +12,17 @@ class Firebase
 
   sig { params(topic_name: String, message_type: String, json_str: String).returns(T.untyped)}
   def send_data(topic_name, message_type, json_str)
-    @fcm.send_to_topic(topic_name, data: {type: message_type, data: json_str})
+    @fcm.send_to_topic(topic_name, data: {type: message_type, content: json_str})
   end
 
   sig { params(topic_name: String, body: String, title: T.nilable(String)).returns(T.untyped)}
   def send_notification(topic_name, body, title = nil)
     @fcm.send_to_topic(topic_name, notification: {title: title, body: body})
+  end
+
+  sig { params(topic_name: String, body: String, message_type: String, 
+    json_str: String, title: T.nilable(String)).returns(T.untyped)}
+  def send_notification_with_data(topic_name, body, message_type, json_str, title = nil)
+    @fcm.send_to_topic(topic_name, notification: {title: title, body: body}, data: {type: message_type, content: json_str})
   end
 end
